5adfebb6818c0e4361c08ac7227955be671e89b7
hashir-umar <hashirumar19@gmail.com>
2018-12-07 03:25:54 +0500

    Added Fragment+Acivity

---
+++ app/src/main/java/com/example/hashir/reminder/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.hashir.reminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
