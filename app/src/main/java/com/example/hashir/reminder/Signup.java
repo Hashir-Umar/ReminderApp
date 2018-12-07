9c136c0a6b08388ae9e33a9557f172bee2bd8850
hashir-umar <hashirumar19@gmail.com>
2018-12-07 20:44:56 +0500

    update Files

--- app/src/main/java/com/example/hashir/reminder/Signup.java
+++ app/src/main/java/com/example/hashir/reminder/Signup.java
@@ -1,26 +1,23 @@
 package com.example.hashir.reminder;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+public class Signup extends Fragment {
 
-public class Login extends Fragment {
-
-
-    public Login() {
+    public Signup() {
+        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-        Toast.makeText(getContext(), "hello", Toast.LENGTH_SHORT).show();
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup, container, false);
     }
-
 }
